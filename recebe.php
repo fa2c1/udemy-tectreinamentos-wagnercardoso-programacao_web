1797182e9a8922673760cfb15a7e457bc5448726
Fernando Coutinho <fa2c1@hotmail.com>
2022-08-06 22:42:16 -0300

    Fix error

--- recebe.php
+++ recebe.php
@@ -44,50 +44,40 @@
 								echo "
 									<h2>Pacote Office</h2><br>
 									<img src='img/office.png' class='img-fluid' alt='Office 365'><br><br>
-
 									Faculdade, cursos livres e técnicos<br>
-
 									Mercado de trabalho disputado, empresas cada vez mais exigentes...<br>
-
 									Tempo e Dinheiro, são fatores que ocupam e preocupam pessoas o tempo todo na atual sociedade.<br>
-
-									Microsoft Office Essencial, apresenta a forma mais simples e rápida de aprender as principais ferramentas da Microsoft para a criação de Planilhas eletrônicas, Formatação de Textos e Criação de Apresentações com os já conhecidos e amplamente utilizados Softwares Excel, Word e Power Point.";
+									Microsoft Office Essencial, apresenta a forma mais simples e rápida de aprender as principais ferramentas da Microsoft para a criação de Planilhas eletrônicas, 
+									Formatação de Textos e Criação de Apresentações com os já conhecidos e amplamente utilizados Softwares Excel, Word e Power Point.";
 								break;
 							case 'negocios':
 								echo "
 									<h2>Negócios Digitais</h2>
 									<img src='img/negocios.png' class='img-fluid' alt='Dinheiro em caixa'><br><br>
-
 									Já imaginou desenvolver um site e obter renda extra com isso?<br>
-
 									Já imaginou trabalhar como afiliado de produtos digitais, gerando renda sem ter que abandonar seu atual emprego ou atividade?<br>
-
 									Prestar serviços de desenvolvimento de sites para empreendedores digitais, com campanhas completas de Marketing digital utilizando Google Ads, Facebook Ads?<br>
-
 									Tornar - se um afiliado Profissional de plataformas conhecidas como Hotmart, Eduzz, Monetizze e Udemy?<br>
-
 									Alavancar a venda de produtos físicos melhorando sua presença digital?<br>
-
 									Se você disse sim para uma (ou todas) essas perguntas, então este curso é ideal!";
 								break;
 							case 'marketing':
 								echo "
 									<h2>Marketing Digital</h2>
 									<img src='img/Ads.png' class='img-fluid' alt='ADS Facebook e Google'><br><br>
-
-									Uma das maiores dificuldades no início de qualquer empreendimento é a conquista dos clientes, mostrar o quanto sua ideia pode ser inovadora ou o quanto seu produto pode ser útil e sua qualidade surpreendente.<br>
-
+									Uma das maiores dificuldades no início de qualquer empreendimento é a conquista dos clientes, mostrar o quanto sua ideia pode ser inovadora ou o quanto seu produto 
+									pode ser útil e sua qualidade surpreendente.<br>
 									Se você é um produtor de conteúdo digital (Youtuber por exemplo) este curso vai ajudar a alavancar seus vídeos e conquistar público. <br>
-
-									Neste curso trabalhamos com o Marketing Direto, no qual os anúncios serão totalmente objetivos levando clientes (que estajam buscando por seu produto) até a sua página de vendas, canal, loja virtual ou física e página do Facebook.<br>";
+									Neste curso trabalhamos com o Marketing Direto, no qual os anúncios serão totalmente objetivos levando clientes (que estajam buscando por seu produto) até a sua 
+									página de vendas, canal, loja virtual ou física e página do Facebook.<br>";
 								break;				
 							default:
-								echo "Infelizmente não encontramos resposta para sua pesquisa, clique em voltar e tente novamente.";
+								echo "Infelizmente não encontramos resposta para sua pesquisa, tente novamente usando as palavras chaves: office, negocios ou marketing.";
 								break;
 						}
 					?>
 					<br><br>
-					<div  style="text-align:center">
+					<div class="send">
 						<a href="index.html" class="btn btn-outline-primary">Voltar</a>
 					</div>	
 				</div>
